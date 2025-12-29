ad3cc92a640d802cb31223c72248c0a5e30997cd
eduar2gp <eduar2gp@gmail.com>
2025-12-28 22:03:19 -0600

    -routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,29 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { isDevMode } from '@angular/core';;
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
+//import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+//import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+//import { provideServiceWorker } from '@angular/service-worker';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { authBlockInterceptor } from './core/interceptors/auth-block.interceptor'
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    // Providers from the first config
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+
+    // Router and HTTP/Chart providers from the second config
+    provideRouter(routes),
+    provideCharts(withDefaultRegisterables()),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([AuthInterceptor, authBlockInterceptor])
+    ), provideCharts(withDefaultRegisterables()),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,44 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component'
+import { AddOrderComponent } from '../app/views/order/add-order/add-order.component'
+import { OrdersListComponent } from './views/order/orders-list/orders-list.component'
+import { roleGuard } from '../app/core/guards/role.guard';
+import { WalletComponent } from '../app/views/wallet/wallet.component'
+import { TransactionsListComponent } from '../app/views/transaction/transactions-list/transactions-list.component'
 
 export const routes: Routes = [
-    {
+   {
     path: '',
     component: LoginComponent,    
-    },
-    {
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+   {
     path: 'dashboard',
-    component: DashboardComponent,    
-    },
+     component: DashboardComponent,
+     canActivate: [roleGuard],     
+  },
+  {
+    path: 'add-order',
+    component: AddOrderComponent,
+    canActivate: [roleGuard],    
+  },
+  {
+    path: 'orders',
+    component: OrdersListComponent,
+    canActivate: [roleGuard],    
+  },
+  {
+    path: 'wallet',
+    component: WalletComponent,
+    canActivate: [roleGuard],
+  },
+  {
+    path: 'transactions',
+    component: TransactionsListComponent,
+    canActivate: [roleGuard],
+  },
 ];
