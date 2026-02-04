1a51106aa19bc4a498952b36b115b19caae64292
eduar2gp <eduar2gp@gmail.com>
2026-02-03 23:14:59 -0600

    -one dashboard -new bottom nav style

--- src/app/views/dashboard/exchange-dashboard/exchange-dashboard.component.ts
+++ src/app/views/dashboard/exchange-dashboard/exchange-dashboard.component.ts
@@ -3,11 +3,12 @@ import { OrderBookComponent } from '../../../views/order/orders-book/orders-book
 import { CandlePriceChartComponent } from '../../../views/charts/candle-chart/candle-price-chart.component'
 import { RecentTradesComponent } from '../../../views/trades/recent-trades.component'
 import { MatTabsModule, MatTabChangeEvent } from '@angular/material/tabs';
+import { EcommerceDashboardComponent } from '../ecommerce-dashboard/ecommerce-dashboard.component'; 
 
 @Component({
   standalone: true,
   selector: 'app-dashboard',
-  imports: [OrderBookComponent, CandlePriceChartComponent, RecentTradesComponent, MatTabsModule],
+  imports: [OrderBookComponent, CandlePriceChartComponent, RecentTradesComponent, MatTabsModule, EcommerceDashboardComponent],
   templateUrl: './exchange-dashboard.component.html',
   styleUrl: './exchange-dashboard.component.css',
 })
